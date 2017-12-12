02ce1b90ae250f764d27a59201a8620208731d7e
Tommy <tommyt6073@gmail.com>
2017-12-12 21:52:18 +0900

    Change Money->equals() to compare currency instead of class

--- Money.php
+++ Money.php
@@ -34,7 +34,7 @@ class Money
      */
     public function equals(Money $money)
     {
-        return ($this->amount == $money->amount) && (get_class($this) === get_class($money));
+        return ($this->amount === $money->amount) && ($this->currency === $money->currency);
     }
 
     /**
