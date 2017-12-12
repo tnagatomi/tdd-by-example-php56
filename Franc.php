662fe036472f0266a5522eb883d3ce96f3c69baa
Tommy <tommyt6073@gmail.com>
2017-12-12 21:53:03 +0900

    Change Franc->times() back to return Money

--- Franc.php
+++ Franc.php
@@ -20,6 +20,6 @@ class Franc extends Money
      */
     public function times($multiplier)
     {
-        return new Franc($this->amount * $multiplier, $this->currency);
+        return new Money($this->amount * $multiplier, $this->currency);
     }
 }
