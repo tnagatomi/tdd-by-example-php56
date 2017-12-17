a5c8fc40ea15e516b453384e5cc3cf88e739d356
Tommy <tommyt6073@gmail.com>
2017-12-17 14:31:49 +0900

    Add Bank class

---
+++ Bank.php
@@ -0,0 +1,14 @@
+<?php
+
+class Bank
+{
+    /**
+     * @param Expression $source
+     * @param string $to
+     * @return Money
+     */
+    public function reduce(Expression $source, $to)
+    {
+        return null;
+    }
+}
