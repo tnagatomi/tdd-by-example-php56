18db28d8db63fbd5a0a1d560f750f7891e59108c
Tommy <tommyt6073@gmail.com>
2017-12-27 19:55:31 +0900

    Finish chapter 23

--- xunit.php
+++ xunit.php
@@ -1,5 +1,32 @@
 <?php
 
+class TestResult
+{
+    public $runCount;
+    public $errorCount;
+
+    public function __construct()
+    {
+        $this->runCount = 0;
+        $this->errorCount = 0;
+    }
+
+    public function testStarted()
+    {
+        $this->runCount++;
+    }
+
+    public function testFailed()
+    {
+        $this->errorCount++;
+    }
+
+    public function summary()
+    {
+        return sprintf('%d run, %d failed', $this->runCount, $this->errorCount);
+    }
+}
+
 class TestCase
 {
     public $name;
@@ -19,11 +46,41 @@ class TestCase
 
     }
 
-    public function run()
+    /**
+     * @param TestResult $result
+     */
+    public function run(TestResult $result)
     {
+        $result->testStarted();
         $this->setUp();
-        call_user_func([$this, $this->name]);
+        try {
+            call_user_func([$this, $this->name]);
+        } catch (Exception $e) {
+            $result->testFailed();
+        }
         $this->tearDown();
+    }
+}
+
+class TestSuit
+{
+    public $tests;
+
+    public function __construct()
+    {
+        $this->tests = [];
+    }
+
+    public function add($test)
+    {
+        $this->tests[] = $test;
+    }
+
+    public function run($result)
+    {
+        foreach ($this->tests as $test) {
+            $test->run($result);
+        }
     }
 }
 
@@ -41,6 +98,11 @@ class WasRun extends TestCase
         $this->log = $this->log . 'test Method ';
     }
 
+    public function testBrokenMethod()
+    {
+        throw new Exception();
+    }
+
     public function tearDown()
     {
         $this->log = $this->log . 'tearDown ';
@@ -50,13 +112,57 @@ class WasRun extends TestCase
 class TestCaseTest extends TestCase
 {
     public $test;
+    public $result;
+
+    public function setUp()
+    {
+        $this->result = new TestResult();
+    }
 
     public function testTemplateMethod()
     {
         $test = new WasRun('testMethod');
-        $test->run();
+        $test->run($this->result);
         assert('setUp test Method tearDown ' === $test->log);
+    }
+
+    public function testResult()
+    {
+        $test = new WasRun('testMethod');
+        $test->run($this->result);
+        assert('1 run, 0 failed' === $this->result->summary());
+    }
+
+    public function testFailedResult()
+    {
+        $test = new WasRun('testBrokenMethod');
+        $test->run($this->result);
+        assert('1 run, 1 failed' === $this->result->summary());
+    }
+
+    public function testFailedResultFormatting()
+    {
+        $this->result->testStarted();
+        $this->result->testFailed();
+        assert('1 run, 1 failed' === $this->result->summary());
+    }
+
+    public function testSuit()
+    {
+        $suit = new TestSuit();
+        $suit->add(new WasRun('testMethod'));
+        $suit->add(new WasRun('testBrokenMethod'));
+        $suit->run($this->result);
+        assert('2 run, 1 failed' === $this->result->summary());
     }
 }
 
-(new TestCaseTest('testTemplateMethod'))->run();
+$suit = new TestSuit();
+$suit->add(new TestCaseTest('testTemplateMethod'));
+$suit->add(new TestCaseTest('testResult'));
+$suit->add(new TestCaseTest('testFailedResult'));
+$suit->add(new TestCaseTest('testFailedResultFormatting'));
+$suit->add(new TestCaseTest('testSuit'));
+$result = new TestResult();
+$suit->run($result);
+echo $result->summary();
